0922d9db85bf2b3c643f1e7a1153c4e465da35d1
Yeicam1020 <yarojasa1020@gmail.com>
2025-05-09 16:53:39 -0500

    Se inicia el proyecto

---
+++ PPB-nequi-pragma/src/main/java/com/prueba/backend/PpbNequiPragmaApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpbNequiPragmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpbNequiPragmaApplication.class, args);
+	}
+
+}
---
+++ PPB-nequi-pragma/src/test/java/com/prueba/backend/PpbNequiPragmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpbNequiPragmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
