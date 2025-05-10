d83d0df01617d64d22f449a1ef3166c26373886f
yeicamrojas <yarojasa2020-1@corhuila.edu.co>
2025-05-09 23:55:10 -0500

    Creacion modulo Service

---
+++ PPB-nequi-pragma/src/main/java/com/prueba/backend/service/FranquiciaService.java
@@ -0,0 +1,42 @@
+package com.prueba.backend.service;
+
+import com.prueba.backend.entidades.Franquicia;
+import com.prueba.backend.repository.FranquiciaRepo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+@Slf4j
+public class FranquiciaService {
+    private final FranquiciaRepo franquiciaRepo;
+
+    public FranquiciaService(FranquiciaRepo franquiciaRepo) {
+        this.franquiciaRepo = franquiciaRepo;
+    }
+
+    public Mono<Franquicia> anadirFranquicia(Franquicia franquicia) {
+        return franquiciaRepo.save(franquicia);
+    }
+
+    public Mono<Franquicia> findById(Long id) {
+        return franquiciaRepo.findById(id);
+    }
+
+    public Mono<Void> eliminarFranquicia(Long id) {
+        return franquiciaRepo.deleteById(id);
+    }
+
+    public Mono<Franquicia> actualizarNombreSucursal(Long id, String newName) {
+        return franquiciaRepo.findById(id)
+                .flatMap(franquicia -> {
+                    franquicia.setNombre(newName);
+                    return franquiciaRepo.save(franquicia);
+                });
+    }
+
+    public Flux<Franquicia> getAllFranquicias() {
+        return franquiciaRepo.findAll();
+    }
+}
